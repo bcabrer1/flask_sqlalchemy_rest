7b30b815f09e548b56159e8788cb617b3c4c4e18
Ben Cabrera <bcabrer1@terpmail.umd.edu>
2022-06-16 19:52:56 -0400

    this is the first draft of database.

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+#init app
+app= Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#init db
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
+# Product Class/Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+# Priduct Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id','name','description', 'price','qty')
+# Init schema
+product_schema = ProductSchema(strict=True)
+products_schema = ProductSchema(many=True, strict=True)
+
+#Create a Product
+if __name__ == '__main__':
+    app.run(debug=True)
